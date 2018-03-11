bf0dade47b8832a44a19874050faf0df0aff805b
Corey Cunningham <coreyjjc@me.com>
2018-03-11 19:43:20 -0400

    session words done

--- survey_form/apps/session_words/views.py
+++ survey_form/apps/session_words/views.py
@@ -2,25 +2,37 @@ from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
 # Create your views here.
 def index(req):
+    if "entries" not in req.session:
+        req.session["entries"] = []
+    print req.session["entries"]
     return render(req,'session_words/index.html')
 
 def add(req):
-    date = strftime("%H:%M:%s %p %M-%d, %Y")
-    word = req.POST["word"]
-    color = req.POST["color"]
+    date = strftime("%r:%M:%S %p %b %d, %Y")
+    try:
+        word = req.POST["word"]
+        color = req.POST["color"]
+    except:
+        print "ERROR must choose color and word"
+        return redirect('/session_words')
+
     if "big" in req.POST:
         big = req.POST["big"]
     else:
         big = False
     if "entries" in req.session:
-        req.session["entries"].append({
+        entries = req.session["entries"]
+        entries.append({
             "date": date,
             "word": word,
             "color": color,
             "big": big
         })
+        req.session["entries"] = entries
     else:
-        req.session["entries"] = []
+        print "error occured"
+        return redirect('/session_words')
+    print req.session["entries"]
     return redirect('/session_words')
 
 def clear(req):
