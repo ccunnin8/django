a56e8d16cba0fb83e656b98dcb5434e3094650e6
Corey Cunningham <coreyjjc@me.com>
2018-04-05 10:37:05 -0400

    underscore

--- python_advanced.py
+++ python_advanced.py
@@ -17,3 +17,33 @@ class MathDojo(object):
             else:
                 self.total -= arg
         return self
+
+class Underscore(object):
+    def map(self,x,y):
+        for index, item in enumerate(x):
+            x[index] = y(item)
+        return x
+
+    def reduce(self,x,y):
+        total = 0
+        for item, val in enumerate(x):
+            total += y(val)
+        return total
+
+    def find(self,x,y):
+        for index,val in enumerate(x):
+            if val == y:
+                return index
+        return -1
+
+    def filter(self,x,y):
+        for index,val in enumerate(x):
+            if not y(val):
+                del x[index]
+        return x
+
+    def reject(self,x,y):
+        for item in x:
+            if type(item) != y:
+                return True
+        return False
